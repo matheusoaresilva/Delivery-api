1c5b1986aad34a70f571be6e6eab54430ed72431
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-22 21:56:30 -0300

    cozinhas post

--- src/main/java/com/matheus/fastfoodie/api/controller/CozinhaController.java
+++ src/main/java/com/matheus/fastfoodie/api/controller/CozinhaController.java
@@ -3,8 +3,15 @@ package com.matheus.fastfoodie.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.fastfoodie.domain.model.Cozinha;
@@ -22,9 +29,22 @@ public class CozinhaController {
 		return repository.listar();
 	}
 	
-	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(Long id) {
-		return repository.buscar(id);
+	@GetMapping("/{id}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable("id") Long id) {
+		Cozinha cozinha = repository.buscar(id);
+		
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return repository.salvar(cozinha);
 	}
 
 }
